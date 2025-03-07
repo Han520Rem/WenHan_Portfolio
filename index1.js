fd333704225d8c6e246d7ad2108185d2b7e2714e
Han520Rem <ngwenhan2000@gmail.com>
2025-03-07 10:16:45 +0800

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,61 @@
+// Sticky Navigation Menu
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+
+// Show/hide sticky navigation and scroll button based on scroll position
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  } else {
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
+};
+
+// Hire me event
+function sendEmail() {
+  window.location.href = "mailto:ngwenhan2000@gmail.com?subject=Job Application&body='Hi Sir/Madam, Thanks for viewing my portfolio. Please let me know if you need more information. Thanks.'";
+}
+
+// Download CV event
+function downloadCV() {
+  const link = document.createElement("a");
+  link.href = "NG WEN HAN_CV.pdf"; // File URL
+  link.download = "NG WEN HAN_CV.pdf"; // File name when downloaded
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+// Side Navigation Menu
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+
+// Open side navigation
+menuBtn.onclick = function () {
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflow = "hidden";
+  scrollBtn.style.pointerEvents = "none";
+};
+
+const hideNavMenu = () => {
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  body.style.overflow = "auto";
+  scrollBtn.style.pointerEvents = "auto";
+};
+
+// Close side navigation
+cancelBtn.onclick = hideNavMenu;
+
+// Close side navigation when a menu link is clicked
+let navLinks = document.querySelectorAll(".menu li a");
+navLinks.forEach((link) => {
+  link.addEventListener("click", hideNavMenu);
+});
